d71107a80df460be141e95e7454704d03bcc611e
Aziz Khambati <aziz.h.khambati@gmail.com>
2019-07-29 20:14:29 +0530

    Remove ts-ignore

--- utils/service-worker/sw.ts
+++ utils/service-worker/sw.ts
@@ -1,31 +1,30 @@
-declare var self: ServiceWorkerGlobalScope
-export {}
+declare var self: ServiceWorkerGlobalScope;
+export {};
 
 declare global {
   interface ServiceWorkerGlobalScope {
     __WB_MANIFEST: Array<{
-      revision?: string
-      url: string
-    }>
+      revision?: string;
+      url: string;
+    }>;
   }
 }
 
-// @ts-ignore
-import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute.mjs'
+import { precacheAndRoute } from "workbox-precaching/precacheAndRoute";
 
-self.addEventListener('message', event => {
-  if (!event.data || !event.data.type) return
-  const port = event.ports[0]
+self.addEventListener("message", event => {
+  if (!event.data || !event.data.type) return;
+  const port = event.ports[0];
   switch (event.data.type) {
-    case 'SKIP_WAITING':
-      self.skipWaiting()
-      break
-    case 'CLIENTS_COUNT':
+    case "SKIP_WAITING":
+      self.skipWaiting();
+      break;
+    case "CLIENTS_COUNT":
       self.clients.matchAll().then(allClients => {
-        port.postMessage(allClients.length)
-      })
-      break
+        port.postMessage(allClients.length);
+      });
+      break;
   }
-})
+});
 
-precacheAndRoute(self.__WB_MANIFEST, {})
+precacheAndRoute(self.__WB_MANIFEST, {});
